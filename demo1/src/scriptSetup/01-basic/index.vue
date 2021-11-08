3095397c800636ae03ebe5e1fa08f6acaae05b17
xiao chen <2714000135@qq.com>
2021-11-08 16:44:57 +0800

    20211108

---
+++ demo1/src/scriptSetup/01-basic/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Vue3正常语法</h1>
+    {{ count }}
+    <button @click="add">+</button>
+  </div>
+</template>
+<!-- vue3 如果用ts，导出时候要用 defineComponent，这俩是配对的，为了类型的审查正确 -->
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    //ref 使其变成响应式
+    const count = ref(2);
+
+    const add = () => {
+    //.value 获取其值  这边会自动提示因为有Volar
+      count.value += 1;
+    };
+    return {
+      count,
+      add
+    };
+  },
+});
+</script>
+
+
+<style scoped>
+</style>
---
+++ demo1/src/scriptSetup/02-components/Foo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Setup语法糖</h1>
+    {{ count }}
+    <button @click="add">+</button>
+  </div>
+</template>
+
+<!-- <script setup> 中的代码会在每次组件实例被创建的时候执行 -->
+<!-- 不再需要使用 return 导出 -->
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(1);
+const add = () => {
+  //.value 获取其值  这边会自动提示因为有Volar
+  count.value += 1;
+};
+</script>
+
+<style scoped>
+</style>
